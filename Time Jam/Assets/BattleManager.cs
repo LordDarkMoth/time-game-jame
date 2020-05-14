36b2f096ef7534c1553116c8f1c0c6c397e78525
LordDarkMoth <famtio@gmail.com>
2020-05-13 19:34:52 -0600

    got battle start and stop structured

---
+++ Time Jam/Assets/BattleManager.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+
+    public List<GameObject> enemiesInBattle = new List<GameObject>();
+    public int enemiesReady = 0;
+    private Vector3 neutralCords = new Vector3(-1f, -10f, -1f);
+    private Vector3 neutralSize = new Vector3(.1f, .1f, .1f);
+
+    private bool battleCommenced = false;
+    private BoxCollider BattleArea;
+    public playerController3d _player;
+    public GameObject playerBody;
+    
+
+
+
+    //opt vars
+    float optFloat;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BattleArea = this.GetComponent<BoxCollider>();
+        
+        BattleArea.size = neutralSize;
+        this.transform.position = neutralCords;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemiesInBattle.Count <=0 && battleCommenced) {
+            //EndBattle();
+        }
+    }
+
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        print("colider searching");
+        if (collision.gameObject.tag == "Enemy")
+        {
+
+            print("enemy found");
+            if (!collision.gameObject.GetComponent<EnemyBasic>().inBattle) {
+                enemiesInBattle.Add(collision.gameObject);
+                collision.gameObject.GetComponent<EnemyBasic>().inBattle = true;
+            }
+
+        }
+    }
+
+    public void SetUpBattle(GameObject initEnemy) {
+
+        Debug.Log("moving battle area Size: " + BattleArea.size + " | center: " + BattleArea.center);
+        BattleArea.size = new Vector3(20f, 2f, 30f);
+        this.transform.position = initEnemy.transform.position;
+        Debug.Log("battle area fixed Size: " + BattleArea.size + " | center: " + BattleArea.center);
+
+        _player.preapareForBattle();
+
+        StartCoroutine(PositionCombatants());
+
+        
+
+    }
+
+    private IEnumerator PositionCombatants() {
+
+        yield return new WaitForSeconds(.5f);
+
+        Vector3 BattleCenter = this.transform.position;
+        float xPosModifer;
+        if (BattleCenter.x > playerBody.transform.position.x) { //enemies to left
+            xPosModifer = -1f;
+        } else {
+            xPosModifer = 1f;
+        
+        }
+        Vector3 CombatantPos = new Vector3(BattleCenter.x + (6f * xPosModifer), 0, BattleCenter.z);
+        playerBody.transform.position = CombatantPos;
+        for (int i=0; i < enemiesInBattle.Count; i++) {
+            float xAdjust = 0f;
+            float zAdjust = 0f;
+
+            switch (i) {
+                case 1:
+                    xAdjust = 2f;
+                    
+                    break;
+                case 2:
+                    xAdjust = 4f;
+                    
+
+                    break;
+                case 3:
+                    xAdjust = 6f;
+                    
+
+                    break;
+                case 4:
+                    xAdjust = 8f;
+                    
+
+                    break;
+                case 5:
+                    xAdjust = 10f;
+                   
+
+                    break;
+                case 6:
+                    xAdjust = 12f;
+                    
+
+                    break;
+                case 7:
+                    xAdjust = 14f;
+                    
+
+                    break;
+                case 8:
+                    xAdjust = 16f;
+                    
+
+                    break;
+                case 9:
+                    xAdjust = 18f;
+                    
+
+                    break;
+            }
+
+            CombatantPos = new Vector3(BattleCenter.x + ((3f + xAdjust) * xPosModifer * -1), 1.5f, BattleCenter.z + zAdjust);
+            enemiesInBattle[i].GetComponent<EnemyBasic>().PrepareForBattle(CombatantPos);
+
+        }
+
+
+
+    }
+    public void StartBattle()
+    {
+        if (enemiesReady >= enemiesInBattle.Count) {
+            print("Battle Started!! with " + enemiesInBattle.Count + " enemies");
+
+            //loop through enemies and make them start making fight decisions
+            battleCommenced = true;
+        }
+        
+
+    }
+
+    public void EndBattle() {
+        Debug.Log("Leaving battle");
+        battleCommenced = false;
+        BattleArea.size = neutralSize;
+        BattleArea.center = neutralCords;
+        _player.leaveBattle();
+        
+
+
+
+    }
+     public void calculateBasicPlayerAttack(float rangeMin, float rangeMax, float damageAug, bool facingLeft)
+    {
+        print("Battle manager handling attack");
+        bool playerFacingLeft = facingLeft;
+        print("Battle manager thinks player facing left is " + playerFacingLeft);
+        print("Checking " + enemiesInBattle.Count + "enemies for range");
+        for (int i = 0; i < enemiesInBattle.Count;i++ )
+        {
+            if (playerFacingLeft && enemiesInBattle[i].transform.position.x < playerBody.transform.position.x) {// facing left and enemy too the left 
+                print("Facing left i found an enemy");
+                optFloat = Vector3.Distance(playerBody.transform.position, enemiesInBattle[i].transform.position);
+
+                print("Enemy proximity is " + optFloat);
+
+                print("Attack Xrange is between " + rangeMin + "-" + rangeMax);
+                if (optFloat < rangeMax && optFloat > rangeMin)
+                {
+                    print("the enemy was in range");
+                    enemiesInBattle[i].GetComponent<EnemyBasic>().takeDamage(damageAug);
+                }
+
+            } else if (!playerFacingLeft && enemiesInBattle[i].transform.position.x > playerBody.transform.position.x)
+            {// facing right and enemy too the right 
+                print("Facing right i found an enemy");
+                optFloat = Vector3.Distance(playerBody.transform.position, enemiesInBattle[i].transform.position);
+
+                print("Enemy proximity is " + optFloat);
+
+                print("Attack Xrange is between " + rangeMin + "-" + rangeMax);
+                if (optFloat < rangeMax && optFloat > rangeMin)
+                {
+                    print("the enemy was in range");
+                    enemiesInBattle[i].GetComponent<EnemyBasic>().takeDamage(damageAug);
+                }
+            }
+
+
+
+
+        }
+
+    }
+
+}
--- Time Jam/Assets/EnemyBasic.cs
+++ Time Jam/Assets/EnemyBasic.cs
@@ -9,10 +9,12 @@ public class EnemyBasic : MonoBehaviour
     private Rigidbody myBody;
     private bool playerInRange = false;
     private int myFacing = 1; // 0: East, 1: West;
-    //public BattleManager _battleManager;
+    private Animator myAnimator;
+    public BattleManager _battleManager;
 
     public int senseLimit = 120;
     private int targetSightedCountdown;
+    private bool battleStartUp = false;
     public bool inBattle = false;
     private GameObject thePlayer;
 
@@ -25,35 +27,87 @@ public class EnemyBasic : MonoBehaviour
     float def = .2f;
     [SerializeField]
     float attack;
+    [SerializeField]
+    float speed = 1f;
+
+
+    //Enemy AI
+    bool travelling;
+    bool leaping;
+    Vector3 startPosition;
+    Vector3 destination;
+    [SerializeField]
+    float destinationReachedThreshold;
+    float destinationProximity;
+    private float journeyStartTime;
+    private float journeyLength;
+    float distCovered;
+    float fractionOfJourney;
+    float moveSpeed;
+
 
 
 
     // Start is called before the first frame update
     void Start()
     {
+        moveSpeed = speed;
+        journeyLength = Vector3.Distance(startPosition, destination);
         senseArea = this.GetComponent<BoxCollider>();
         myBody = this.GetComponent<Rigidbody>();
-
         targetSightedCountdown = senseLimit;
+        myAnimator = this.GetComponent<Animator>();
+        destination = this.transform.position;
     }
 
+    private void Update()
+    {
+        if (travelling) {
+            distCovered = (Time.time - journeyStartTime) * moveSpeed;
+            fractionOfJourney = distCovered / journeyLength;
+            transform.position = Vector3.Lerp(startPosition, destination, fractionOfJourney);
+
+            destinationProximity = Mathf.Abs(transform.position.x - destination.x) + Mathf.Abs(transform.position.y - destination.y) + Mathf.Abs(transform.position.y - destination.y);
+
+            if (destinationProximity < destinationReachedThreshold)
+            {
+                reachDestination();
+
+            }
+        }
+        
+    }
     private void FixedUpdate()
     {
-        if (playerInRange) {
-            if (targetSightedCountdown > 0) {
-                targetSightedCountdown--;
-                if (myFacing == 0 && thePlayer.transform.position.x > this.transform.position.x || myFacing == 1 && thePlayer.transform.position.x < this.transform.position.x) {
-                    targetSightedCountdown--;
-                }
-            } else if(!inBattle)
+        
+
+
+        if (!inBattle) {
+            if (playerInRange)
             {
-                //_battleManager.SetUpBattle(this.gameObject);
+                if (targetSightedCountdown > 0)
+                {
+                    targetSightedCountdown--;
+                    if (myFacing == 0 && thePlayer.transform.position.x > this.transform.position.x || myFacing == 1 && thePlayer.transform.position.x < this.transform.position.x)
+                    {
+                        targetSightedCountdown--;
+                    }
+                }
+                else
+                {
+                    
+                    inBattle = true;
+                    _battleManager.SetUpBattle(this.gameObject);
+                }
+
+
             }
-
-
-        } else if (targetSightedCountdown < senseLimit) {
-            targetSightedCountdown++;
+            else if (targetSightedCountdown < senseLimit)
+            {
+                targetSightedCountdown++;
+            }
         }
+
 
 
     }
@@ -62,6 +116,7 @@ public class EnemyBasic : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter(Collider collision)
     {
+        Debug.Log("Feild entered");
         if (collision.gameObject.tag == "Player")
         {
             //print("Player Hit");
@@ -72,7 +127,7 @@ public class EnemyBasic : MonoBehaviour
     }
     private void OnTriggerExit(Collider collision)
     {
-
+        Debug.Log("Feild exit");
         if (collision.gameObject.tag == "Player")
         {
             //print("Player Left");
@@ -81,10 +136,73 @@ public class EnemyBasic : MonoBehaviour
         }
     }
 
+    public void alterFacing(Vector3 LookToPoint) {
+        if (this.transform.position.x > LookToPoint.x)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
+    public void moveToLocation(Vector3 where, float speedAug = 1){
+        //check to look the right way
+        alterFacing(where);
+        // trigger correct animation
+        if (leaping)
+        {
+            myAnimator.SetTrigger("Leap");
+        }
+        else if(inBattle){
+            myAnimator.SetTrigger("BattleRun");
+        }
+        else
+        {
+            myAnimator.SetTrigger("Walk");
+        }
+        // lerp to destination
+        journeyStartTime = Time.time;
+        startPosition = transform.position;
+        destination = where;
+        moveSpeed = speedAug * moveSpeed;
+        travelling = true;
+
+    }
+
+    public void reachDestination() {
+        moveSpeed = speed;
+        travelling = false;
+        if (leaping) {
+            myAnimator.SetTrigger("Land");
+            leaping = false;
+            if (battleStartUp) {
+                _battleManager.enemiesReady++;
+                _battleManager.StartBattle();
+                battleStartUp = false;
+            }
+                
+        }
+        else if (inBattle)
+        {
+            myAnimator.SetTrigger("BattleIdle");
+        }
+        else {
+            myAnimator.SetTrigger("Rest");
+        }
+    }
+    public void PrepareForBattle(Vector3 battleStartPosition) {
+        senseArea.enabled = false;
+        leaping = true;
+        moveToLocation(battleStartPosition,3);
+
+    }
     public void takeDamage(float rawDamage) {
         float DamgeDealt = rawDamage - (rawDamage * def);
         hp -= DamgeDealt;
-        print("took " + DamgeDealt + " damage, now hp at " + hp);
+        myAnimator.SetTrigger("TakeDamage");
+
         if (hp <=0) {
             getKilled();
         }
@@ -92,7 +210,13 @@ public class EnemyBasic : MonoBehaviour
 
     private void getKilled() {
         print("I died!");
-        //_battleManager.enemiesInBattle.Remove(gameObject);
+        myAnimator.SetTrigger("Die");
+        
+    }
+    public void RemoveMe() {
+        if(_battleManager != null)
+            _battleManager.enemiesInBattle.Remove(gameObject);
+
         Destroy(gameObject);
     }
 }
